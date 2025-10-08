7c2bfbed6207c34d6f0fcaa5e576f4b0b5e0b382
giuliana saganea <giuliana.saganea@ipat.edu.ar>
2025-10-08 12:22:41 -0300

    added router and model

---
+++ models/modelRecipes.php
@@ -0,0 +1,50 @@
+<?php
+require_once './config.php';
+
+class modelRecipes {
+    private $db;
+
+    public function __construct() {
+        $this-> db = new PDO('mysql:host='. MYSQL_HOST .';dbname='. MYSQL_DB .';charset=utf8', MYSQL_USER, MYSQL_PASS);
+    }
+
+    public function showRecipes(){
+
+        $query = $this->db->prepare('SELECT * FROM recipes');
+        $query = execute();
+        $recipes = $query->fetchAll(PDO::FETCH_OBJ);
+        return $recipes;
+    }
+
+     public function showRecipeById($id){
+        //mostrar receta por id usuario?
+        $query = $this->db->prepare('SELECT * FROM recipes WHERE id_recipe = ? ORDER BY id_recipe ASC');
+        $query->execute([$id]);
+
+        $recipes = $query->fetchAll(PDO::FETCH_OBJ);
+        return $recipes;
+    }
+
+    public function deleteRecipe($id){
+
+        $query = $this->db->prepare('DELETE FROM recipes WHERE id_user'); //chequear esto
+        $query = execute();
+    }
+
+    public function createRecipe($title,$content,$time,$date,$id_user){
+
+        $query = $this->db->prepare('INSERT INTO recipes(title, content, time, date, id_user) VALUES (?,?,?,?,?)');
+        $query->execute([$title, $content, $time, $date, $id_user]);
+        // return $this->db->lastInsertId() por las dudas, y chequear opcionales! tiene que tener 
+    }
+
+    public function updateRecipe($id_recipe,$title,$content,$time,$date,$id_user){
+
+        $query = $this->db->prepare('UPDATE recipes SET title = ?, content = ?, time = ?, date = ?, id_user = ? WHERE id_recipe = ?');
+        $query->execute([$id_recipe,$title,$content,$time,$date,$id_user]);
+    }
+
+
+
+
+}
---
+++ router.php
@@ -0,0 +1,48 @@
+<?php
+require_once './controllers/controllerRecipes.php';
+require_once './controllers/controllerUsers.php';
+
+
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+$action = 'home';
+
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+$params = explode('/', $action);
+switch ($params[0]) {
+    case "home": //por defecto
+        $controller = new controllerRecipes;
+        $controller->showInicio(); //tengo que hacerlo
+        break;
+    
+    case "showRecipes": //muestro todas
+        $controller = new controllerRecipes; //lamo a controller de recipes
+        $controller->showRecipes(); //llamo a funcion especifica
+        break;
+
+    case "showRecipeById": //muestro receta por id
+        $controller = new controllerRecipes;
+        $controller->showRecipeById($params[1]);
+        break;
+
+    case "deleteRecipe":
+        $controller = new controllerRecipes;
+        $controller->deleteRecipe($params[1]);
+        break;
+    
+    case "updateRecipe":
+        $controller = new controllerRecipes;
+        $controller->updateRecipe($params[1]);
+        break;
+
+    case "createRecipe":
+        //form
+        break;
+
+    default: //en caso de que no sea ninguna muestro error
+        echo ("Error")
+        break;
+}
